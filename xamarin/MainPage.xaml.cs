0985d43298efbbba14d89188da42f95fe6a966be
Kacper Matlag <matlagkacper@gmail.com>
2024-11-05 14:25:51 +0100

    Dodaj pliki projektów.

---
+++ xamarin/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin {
+    public partial class MainPage :ContentPage {
+        int[] maxAges = { 18, 20, 9 };
+        int index = 0;
+        public MainPage() {
+            InitializeComponent();
+            Gatunki.ItemsSource = new List<string>() {
+                "Pies", "Kot","Świnka Morska"
+            };
+        }
+
+        private async void Accept_Clicked(object sender, EventArgs e) {
+            await DisplayAlert("Zapisana Wizyta", $"{Name.Text}, {Gatunki.SelectedItem as string}, {Math.Round(WiekSlider.Value)}, {Purpose.Text}, {Time.Time}", "OK");
+        }
+
+        private void Gatunki_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
+            index = e.SelectedItemIndex;
+            WiekSlider.Maximum = maxAges[index];
+            WiekSlider.Value = 0;
+        }
+
+        private void WiekSlider_ValueChanged(object sender, ValueChangedEventArgs e) {
+            AgeText.Text = "Ile masz lat? " + Math.Round(e.NewValue);
+        }
+    }
+}
